765cf2b320e30e26302668ecb74f451c2c6add80
TrianDii <134402856+CookieTaberu@users.noreply.github.com>
2024-12-10 11:49:03 +0700

    testing

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+<body>
+    <div type="container">
+        <center><h2>Our Wardobe Indonesia</h2></center>
+    </div>
+    <div class="dropdown-menu-md-right " style="width: auto" id="SideBar">
+        <button></button>
+    </div>
+</body>
+</html>
