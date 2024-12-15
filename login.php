9ccec19ad45b69fac5c84416d8e0bfc6bb6bb2fc
Cing2PO <at113928@gmail.com>
2024-12-15 08:37:27 +0700

    login dan register

--- login.php
+++ login.php
@@ -1,27 +1,50 @@
-<?php
-
-//require "loginService.php";?>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.css">
+    <!-- Load Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
 
 <body>
-<h1 class="d-flex align-content-center justify-content-center" style="color: #C6E385" >Login</h1>
-<from>
-    <div class="form-group" style="background-color:">
-        <div class="form-group col-md-6">
-            <label for="Email">Email</label>
-            <input type="email" class="form-control" placeholder="Email" id="Email">
+<div class="container mt-5">
+        <center><h2>Registrasi akun</h2></center>
+        <br>
+        <div class="row justify-content-md-center">
+            <div class="col-md-6">
+                <!-- Form Section -->
+                <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                    <!-- NIM Input -->
+                    <div class="form-group">
+                        <label for="email">email</label>
+                        <input type="text" class="form-control" name="email" id="email" placeholder="abcd@gmail.com" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">password</label>
+                        <input type="Password" class="form-control" name="password" id="password" placeholder="password" required>
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-primary mb-3">Register</button>
+                </form>
+            </div>
         </div>
-        <div class="form-group col-md-6">
-            <label for="Password">Password</label>
-            <input type="password" class="form-control" placeholder="Password" id="Email">
-        </div>
-        <button type="submit" class="btn" style="background: #C6E385; color: white"> Login </button>
-    </div>
-</from>
 </body>
+<?php
+require "koneksi.php";
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM user WHERE email = '$email' AND password = '$password'";
+    $hasil = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($hasil) > 0) {
+        header("Location: dashboard.php");
+    } else {
+        echo "Maaf email atau password salah";
+    }
+}
+?>
 </html>
